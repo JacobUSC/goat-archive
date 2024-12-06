37e9b796bbba4070737e33ab42ee8cdff6976b8e
Jacob <jrr18@email.sc.edu>
2024-12-06 16:17:29 -0500

    uploading files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # goat-archive
-archive of my yugioh website with node js and mongo db
+archive of my yugioh website with node js and mongo db. This website 
+<br>
+<br>
+This was created for a school assignment at the University of South Carolina CSCE 242 course.
+<br>
+<br>
+The backend code can be found [here](https://github.com/JacobUSC/goat-server)
+
---
+++ script.js
@@ -0,0 +1,26 @@
+/**
+ * Author: Jacob Russell
+ * Description: Nav Menu
+ * For: every page
+ */
+
+//css root variable
+const root = document.querySelector(":root");
+
+const showOverlay = () => {
+	root.style.setProperty("--show-overlay", "block");
+};
+
+const showNav = () => {
+	showOverlay();
+	root.style.setProperty("--menu", "block");
+};
+
+const hideNav = () => {
+	root.style.setProperty("--menu", "none");
+	root.style.setProperty("--show-overlay", "none");
+};
+
+document.getElementById("nav-button").onclick = showNav;
+document.getElementById("close-nav-button").onclick = hideNav;
+document.getElementById("transparent-overlay").onclick = hideNav;
